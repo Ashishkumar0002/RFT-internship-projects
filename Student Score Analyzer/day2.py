019594db8560f2b12b8ca0924bf39a54e30b9600
Ashish Kumar <208913855+Ashishkumar0002@users.noreply.github.com>
2026-04-09 15:37:23 +0530

    Update day2.py

--- Student Score Analyzer/day2.py
+++ Student Score Analyzer/day2.py
@@ -1,1 +1,22 @@
+marks = [78, 85, 90, 67, 85, 92, 78]
 
+def analyze(marks):
+    avg = sum(marks) / len(marks)
+    
+    return {
+        "Average": avg,
+        "Highest": max(marks),
+        "Lowest": min(marks),
+        "Above Avg": sum(1 for m in marks if m > avg),
+        "Grades": {
+            "A": sum(1 for m in marks if m >= 90),
+            "B": sum(1 for m in marks if 75 <= m < 90),
+            "C": sum(1 for m in marks if 50 <= m < 75),
+            "F": sum(1 for m in marks if m < 50),
+        }
+    }
+
+result = analyze(marks)
+
+for k, v in result.items():
+    print(k, ":", v)
