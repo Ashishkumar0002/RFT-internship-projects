d5bf93ad9b258842181b3c7f7bfbafdd4392a43b
Ashish Kumar <208913855+Ashishkumar0002@users.noreply.github.com>
2026-04-14 20:08:33 +0530

    Update Day5.py

--- basic CSV Rander/Day5.py
+++ basic CSV Rander/Day5.py
@@ -1,1 +1,25 @@
+import csv
 
+
+def read_csv(file_name):
+    data = []
+
+    with open(file_name, "r", newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            row["Age"] = int(row["Age"])
+            row["Marks"] = int(row["Marks"])
+            data.append(row)
+
+    return data
+
+
+def average_marks(data):
+    return sum(d["Marks"] for d in data) / len(data)
+
+
+# Run
+result = read_csv("students_data_real_names.csv")
+
+print(result)
+print("Average Marks:", average_marks(result))
