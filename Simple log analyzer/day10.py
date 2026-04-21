dbadc44d7da1a6f83fb0d0710d52a357260b0dc5
Ashish Kumar <208913855+Ashishkumar0002@users.noreply.github.com>
2026-04-21 16:48:52 +0530

    Update day10.py

--- Simple log analyzer/day10.py
+++ Simple log analyzer/day10.py
@@ -1,1 +1,26 @@
+logs = [
+    "ERROR DISK FULL",
+    "INFO STARTED",
+    "ERROR FILE MISSING",
+    "WARNING MEMORY LOW"
+]
 
+def analyze_logs(logs):
+    count = {"ERROR": 0, "INFO": 0, "WARNING": 0}
+
+    for log in logs:
+        log = log.upper()  # ignore case
+        word = log.split()[0]  # get log type (first word)
+
+        if word in count:
+            count[word] += 1
+
+    most = max(count, key=count.get)
+
+    return count, most
+
+
+counts, most = analyze_logs(logs)
+
+print("Counts:", counts)
+print("Most Frequent:", most)
